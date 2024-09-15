7c97b48ac0f4076913b87d1b39965e7e2196c832
Sean Kim <seankim.hahjean@gmail.com>
2024-09-15 00:55:56 -0400

    connected fileupload component to MongoDB!

--- frontend/kindle_highlights/src/pages/Dashboard.jsx
+++ frontend/kindle_highlights/src/pages/Dashboard.jsx
@@ -1,14 +1,49 @@
-import { useEffect, useState } from "react";
+import { FileUpload } from "primereact/fileupload";
 
-function Dashboard() {
+const Dashboard = () => {
+  const onUpload = (event) => {
+    console.log("Files uploaded:", event.files);
+  };
+
   return (
     <div>
       <h1>
         This is the dashboard where you will be able to see all of your kindle
         highlights
       </h1>
+
+      <div>
+        <h1>Upload File</h1>
+        <FileUpload
+          name="file"
+          url="http://localhost:5000/upload" //Backend URL
+          accept="*/*"
+          maxFileSize={1000000}
+          customUpload
+          auto
+          chooseLabel="Choose File"
+          uploadLabel="Upload"
+          cancelLabel="Cancel"
+          uploadHandler={(e) => {
+            const formData = new FormData();
+            formData.append("file", e.files[0]); //Append file to form data
+
+            fetch("http://localhost:5000/upload", {
+              method: "POST",
+              body: formData,
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                console.log("File upload successful", data);
+              })
+              .catch((eror) => {
+                console.error("Error uploading file", error);
+              });
+          }}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
